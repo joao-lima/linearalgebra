cc4ae1d59051e24604f4f7ba719426799d15610a
Joao Lima <Joao.Lima@imag.fr>
2010-10-20 10:43:58 +0200

    add

---
+++ memory/no_kernel/add_kernel.cu
@@ -0,0 +1,36 @@
+#ifndef _ADD_KERNEL_H_
+#define _ADD_KERNEL_H_
+
+// Thread block size
+#define BLOCK_SIZE 64
+#ifndef DEVICE
+#define DEVICE	0
+#endif
+
+__global__ void add_one( float *data, unsigned int nblock )
+{
+#if 0
+	int index = (blockIdx.x*blockDim.x + threadIdx.x)*nblock;
+	int i;
+	float x;
+	for( i= 0; i < nblock; i++ ){
+		x= data[index+i];
+		data[index+i] = x;
+	}
+#endif
+}
+
+
+__host__ int check( const float *data, const float v, const unsigned int n )
+{
+	unsigned int i;
+
+	for( i= 0; i < n; i++ )
+		if( data[i] != v ){
+			fprintf(stdout,"%d %f\n",i,data[i]);
+			return 0;
+		}
+	return 1;
+}
+
+#endif // #ifndef _ADD_KERNEL_H_
---
+++ memory/no_kernel_HtoD/pinned.cu
@@ -0,0 +1,71 @@
+
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "add_kernel.cu"
+#include "cuda_safe.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Program main
+////////////////////////////////////////////////////////////////////////////////
+int
+main(int argc, char** argv)
+{
+	unsigned int mem_size= (1 << 25);
+	float elapsed_time_in_Ms= 0;
+	float bandwidth_in_MBs= 0;
+	unsigned int i, max_iter= 10;
+	float *h_data, *d_data;
+
+	if( argc > 1 )
+		mem_size =  (1 << atoi(argv[1]));
+
+	unsigned int nelem= mem_size/sizeof(float);
+	int deviceCount;
+	cudaGetDeviceCount(&deviceCount);
+
+	for( int d= 0; d < deviceCount; d++ ) {
+	cudaSetDevice( d );
+	/* CUDA flags:
+	cudaHostAllocDefault, cudaHostAllocPortable, cudaHostAllocMapped,
+	cudaHostAllocWriteCombined */
+	unsigned int flags= cudaHostAllocDefault;
+	// allocate host memory for matrices A and B
+	CUDA_SAFE_CALL( cudaHostAlloc( (void**)&h_data, mem_size, flags ) );
+	for( i= 0; i < nelem; i++) h_data[i]= 1e0f;
+	// allocate device memory
+	CUDA_SAFE_CALL( cudaMalloc((void**)&d_data, mem_size) );
+	cudaEvent_t e1, e2;
+	cudaEventCreate( &e1 );
+	cudaEventCreate( &e2 );
+
+	CUDA_SAFE_CALL( cudaEventRecord( e1, 0 ) );
+	for( i= 0; i < max_iter; i++ ){
+		CUDA_SAFE_CALL( cudaMemcpy( d_data, h_data, mem_size,
+				      cudaMemcpyHostToDevice) );
+	}
+	CUDA_SAFE_CALL( cudaEventRecord( e2, 0 ) );
+	CUDA_SAFE_CALL( cudaEventSynchronize( e2 ) );
+	CUDA_SAFE_CALL( cudaEventElapsedTime( &elapsed_time_in_Ms, e1, e2 ) );
+	bandwidth_in_MBs= 1e3f * max_iter * mem_size / 
+	       	(elapsed_time_in_Ms * (float)(1 << 20));
+	fprintf( stdout, "pinned gpu= %d size(MB)= %9u time(ms)= %.3f bandwidth(MB/s)= %.1f\n",
+		d, mem_size/(1<<20), elapsed_time_in_Ms/(max_iter),
+	       	bandwidth_in_MBs );
+
+	if( check( h_data, 1e0f, nelem) == 0 )
+		fprintf( stdout, "test FAILED\n" );
+
+	// clean up memory
+	CUDA_SAFE_CALL( cudaEventDestroy( e1 ) );
+	CUDA_SAFE_CALL( cudaEventDestroy( e2 ) );
+	CUDA_SAFE_CALL( cudaFreeHost( h_data ) );
+	CUDA_SAFE_CALL( cudaFree( d_data ) );
+	}
+
+	cudaThreadExit();
+}
+
